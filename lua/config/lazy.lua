d5f7393ae2e068c6cc1b9d14a29faca5c3f0f3c4
FrankXu <frankxu@nine-yi.com>
2024-09-11 09:22:54 +0800

    加入 copilot 預設 lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins/copilot" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
